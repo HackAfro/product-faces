06a5b99e76619df9df7f5ba1c57bb0029ec0212f
Richard Afro <rumoffia@tradedepot.co>
2018-12-26 09:11:10 +0100

    Update entry app file

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import ProductList from './src/products';
+import Header from './src/header';
+export default class App extends React.Component {
+  state = {
+    sortBy: '',
+  };
+
+  onSortValueChanged = (value) => {
+    this.setState({
+      sortBy: value,
+    });
+  };
+
+  render() {
+    const { sortBy } = this.state;
+    return (
+      <View style={styles.container}>
+        <Header onSortValueChanged={this.onSortValueChanged} />
+        <ProductList sortBy={sortBy} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    fontFamily: 'arimo',
+  },
+});
